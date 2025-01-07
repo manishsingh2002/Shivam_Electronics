ca3ce5f8463724cb146eb955f4d23507bd107899
manish5476 <msms5476@gmail.com>
2025-01-07 23:22:25 +0530

    manish-baisc implementation completed

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -4,10 +4,19 @@ import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { DrawerModule } from 'primeng/drawer';
 import { CommonModule } from '@angular/common';
+import { TieredMenu } from 'primeng/tieredmenu';
+import { MenuItem } from 'primeng/api';
+import { Button } from 'primeng/button';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [ ButtonModule, CommonModule, DrawerModule, InputTextModule],
+  imports: [
+    TieredMenu,
+    ButtonModule,
+    CommonModule,
+    DrawerModule,
+    InputTextModule,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
@@ -33,5 +42,78 @@ export class HeaderComponent {
   showDialog(position: string) {
     this.position = position;
     this.visible = true;
+  }
+  items: MenuItem[] | undefined;
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'File',
+        icon: 'pi pi-file',
+        items: [
+          {
+            label: 'New',
+            icon: 'pi pi-plus',
+            items: [
+              {
+                label: 'Document',
+                icon: 'pi pi-file',
+              },
+              {
+                label: 'Image',
+                icon: 'pi pi-image',
+              },
+              {
+                label: 'Video',
+                icon: 'pi pi-video',
+              },
+            ],
+          },
+          {
+            label: 'Open',
+            icon: 'pi pi-folder-open',
+          },
+          {
+            label: 'Print',
+            icon: 'pi pi-print',
+          },
+        ],
+      },
+      {
+        label: 'Edit',
+        icon: 'pi pi-file-edit',
+        items: [
+          {
+            label: 'Copy',
+            icon: 'pi pi-copy',
+          },
+          {
+            label: 'Delete',
+            icon: 'pi pi-times',
+          },
+        ],
+      },
+      {
+        label: 'Search',
+        icon: 'pi pi-search',
+      },
+      {
+        separator: true,
+      },
+      {
+        label: 'Share',
+        icon: 'pi pi-share-alt',
+        items: [
+          {
+            label: 'Slack',
+            icon: 'pi pi-slack',
+          },
+          {
+            label: 'Whatsapp',
+            icon: 'pi pi-whatsapp',
+          },
+        ],
+      },
+    ];
   }
 }
