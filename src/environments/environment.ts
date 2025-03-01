6cc846a3e57c636040de3a34d1c8f2916a489351
manish5476 <msms5476@gmail.com>
2025-03-01 14:35:15 +0530

    manish web tesitn hanges

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:4004/api'
-  // apiUrl: 'https://shivamelectronicsbackend.onrender.com/api'
+  // apiUrl: 'http://localhost:4004/api'
+  apiUrl: 'https://shivamelectronicsbackend.onrender.com/api'
 };
